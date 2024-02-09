7997afd48bfbdd456ea7381a0aa1660e7c86100f
Steven Noreyko <1909661+okyeron@users.noreply.github.com>
2024-02-08 20:51:45 -0600

    Update ttymidi.c

--- src/ttymidi.c
+++ src/ttymidi.c
@@ -310,12 +310,11 @@ void write_midi_action_to_serial_port(snd_seq_t* seq_handle)
 
 			case SND_SEQ_EVENT_SONGPOS:
 				bytes[0] = 0xF2;
-				bytes[1] = (int)ev->data.control.param;
-				bytes[2] = (int)ev->data.control.value;
+				bytes[1] = ev->data.control.param;
+				bytes[2] = ev->data.control.value;
 				if (!arguments.silent && arguments.verbose)
-					printf("Alsa    0x%02X Song Position Pointer  %03u %03u %03u\n", bytes[0]&0xF0, bytes[0]&0xF, bytes[1], bytes[2]);
+					printf("Alsa    0x%02X Song Position Pointer  %03u %03u %03u\n", bytes[0], bytes[0]&0xF, bytes[1], bytes[2]);
 				break;
-
 
             case SND_SEQ_EVENT_SYSEX:
                 sysex_len = ev->data.ext.len;
